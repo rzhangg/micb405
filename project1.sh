ad92f2466752e67b0ac827673878bfdd6f5aabc3
Zhang <u7x9a@ugrad.cs.ubc.ca>
2018-10-19 11:11:48 -0700

    updated with more threads

--- project1.sh
+++ project1.sh
@@ -15,10 +15,18 @@ cp $DIRECTORY/ref_genome.fasta $HOME
 bwa index $HOME/ref_genome.fasta 
 REFERENCE = $HOME/ref_genome.fasta
 cd $HOME
-
-for f in $DIRECTORY/*1.fastq.gz; do prefix=$( basename $f | sed 's/_1.fastq.gz//g' );
-echo $prefix;
-bwa mem $HOME/ref_genome.fasta $DIRECTORY/$prefix\_1.fastq.gz $DIRECTORY/$prefix\_2.fastq.gz | samtools view -b > $BAM/$prefix.bam; 
- 
-#> $SAM/$prefix.sam;
-done
+if [ "$(ls -A $BAM)" ]; then
+    for f in $BAM/*.bam; do 
+        prefix=$( basename $f | sed 's/bam//g' );
+        echo $prefix;
+        samtools sort $BAM/$prefix.bam -o $BAM/$prefix.sorted;
+        samtools index $BAM/$prefix.sorted;
+    done
+else
+    for f in $DIRECTORY/*1.fastq.gz; do prefix=$( basename $f | sed 's/_1.fastq.gz//g' );
+        echo $prefix;
+        bwa mem -t 4 $HOME/ref_genome.fasta $DIRECTORY/$prefix\_1.fastq.gz $DIRECTORY/$prefix\_2.fastq.gz | samtools view -b > $BAM/$prefix.bam; 
+        samtools sort $BAM/$prefix.bam -o $BAM/$prefix.sorted;
+        samtools index $BAM/$prefix.sorted;
+    done
+fi
