942c77ce0af376c19a31a9e3447ac895d326c2b5
apookpun <44304255+apookpun@users.noreply.github.com>
2018-10-27 11:17:36 -0700

    enter something

---
+++ shell/fastqc.sh
@@ -0,0 +1,12 @@
+mkdir fastqc
+DIRECTORY=/projects/micb405/resources/project_1
+USER=$1
+HOME=/home/$USER/project1
+FASTQC=/home/$USER/fastqc
+cd $HOME
+
+    for f in $DIRECTORY/*1.fastq.gz; do prefix=$( basename $f | sed 's/_1.fastq.gz//g' );
+        echo $prefix;
+        fastqc --threads 20 -o $FASTQC $DIRECTORY/$prefix\_?.fastq.gz
+
+    done
